47fec442d0f0caeec646511b51fd805d92102514
0xcoda <tech.codemojo@gmail.com>
2025-09-01 00:36:14 +0100

    raiseboxfaucet v2: added natspec, updated owner functions and more test suite

--- script/DeployRaiseBox.s.sol
+++ script/DeployRaiseBox.s.sol
@@ -1,23 +1,15 @@
 //SPDX-Lincense-Identifier: MIT
 pragma solidity ^0.8.30;
+
 import {Script} from "forge-std/Script.sol";
 import {RaiseBoxFaucet} from "../src/RaiseBoxFaucet.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
 
 contract DeployRaiseboxContract is Script {
-    RaiseBoxFaucet public contractAddress;
+    RaiseBoxFaucet public raiseBox;
+
     function run() public {
         vm.startBroadcast();
-        contractAddress = new RaiseBoxFaucet(
-            "raiseboxtoken",
-            "RB",
-            1000 * 10 ** 18,
-            0.01 ether,
-            1 ether
-        );
+        raiseBox = new RaiseBoxFaucet("raiseboxtoken", "RB", 1000 * 10 ** 18, 0.005 ether, 1 ether);
         vm.stopBroadcast();
     }
 }
-
-
